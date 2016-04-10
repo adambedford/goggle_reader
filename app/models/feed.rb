a0f3e29cef8f3cef138c05e00416b173a1223f38
Adam Bedford <abedford@mavenlink.com>
2016-04-09 18:02:34 -0700

    Implement `Feed#name` to display either the cached feed title or a truncated URL

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,7 @@
 class Feed < ActiveRecord::Base
   validates :url, presence: true
+
+  def name
+    cached_title || url.truncate(50)
+  end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe Feed, type: :model do
   describe "validations" do
     it { should validate_presence_of(:url)}
   end
+
+  describe "#name" do
+    context "when the cached title is present" do
+      subject do
+        create(:feed).tap do |feed|
+          feed.cached_title = "BBC News Feed"
+        end
+      end
+
+      it "returns the cached title" do
+        expect(subject.name).to eq "BBC News Feed"
+      end
+    end
+
+    context "when the cached title is not present" do
+      context "when the URL is longer than 50 characters" do
+        it "gets truncated with ellipses appended" do
+          expect(subject.name).to eq(subject.url.truncate(50))
+        end
+      end
+
+      context "when the URL is shorter than 50 characters" do
+        subject { Feed.create(url: "http://short.url/feed") }
+
+        it "does not get truncated" do
+          expect(subject.name).to eq(subject.url)
+        end
+      end
+    end
+  end
 end
