bca8fbb5f4a658fd56642845b95a753069bbc65a
Adam Bedford <abedford@mavenlink.com>
2016-04-09 22:47:20 -0700

    Configure VCR and move some Rspec config from `spec_helper` to `rails_helper`

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'factory_girl'
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -32,6 +33,8 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -62,4 +65,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Include FactoryGirl methods so you can simply do `create()` instead of
+  # `FactoryGirl.create()`
+  config.include FactoryGirl::Syntax::Methods
 end
