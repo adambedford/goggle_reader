5230a3adb7f0abc871b83e411a7d4e6a951318b5
Adam Bedford <abedford@mavenlink.com>
2016-04-10 22:28:49 -0700

    Configure VCR to re-record cassettes after a week to ensure the RSS response data is accurate

--- spec/support/vcr_support.rb
+++ spec/support/vcr_support.rb
@@ -4,6 +4,9 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.allow_http_connections_when_no_cassette = true
   c.hook_into :webmock
-  c.default_cassette_options = { record: :new_episodes}
+  c.default_cassette_options = {
+    record: :new_episodes,
+    re_record_interval: 1.week
+  }
   c.configure_rspec_metadata!
 end
