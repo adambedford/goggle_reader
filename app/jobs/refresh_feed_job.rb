f4bd3ca47b743a7281581a571df418cc4946e907
Adam Bedford <abedford@mavenlink.com>
2016-04-12 15:42:24 -0700

    Add stdout logging to RefreshFeedJob

--- app/jobs/refresh_feed_job.rb
+++ app/jobs/refresh_feed_job.rb
@@ -2,9 +2,13 @@ class RefreshFeedJob < ActiveJob::Base
   queue_as :default
 
   def perform(feed)
+    puts "START: Refreshing Feed #{feed.id}"
+
     ArticleSync.new(feed).sync
     feed.last_refresh_at = Time.now
     feed.save
+
+    puts "DONE: Refreshing Feed #{feed.id}"
   rescue
     puts "Error refreshing feed #{feed.id}"
   end
