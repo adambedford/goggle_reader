48df0b811455ce914a3c024634767fe910734ddd
Adam Bedford <abedford@mavenlink.com>
2016-04-13 22:42:12 -0700

    Rearrange some layouts, specifically related to the side bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :load_feeds, only: [:index, :show]
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
   end
@@ -14,11 +12,4 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
   helper_method :user_signed_in?
-
-  protected
-
-  def load_feeds
-    @feeds = current_user.feeds
-  end
-
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
 
     respond_to do |format|
-      if @bookmarked_article = @article.bookmark_for_user(current_user)
+      if (@bookmarked_article = @article.bookmark_for_user(current_user))
         format.html { redirect_to :back, notice: "Article bookmarked" }
         format.js { render partial: "feeds/article", locals: { article: @article } }
       else
